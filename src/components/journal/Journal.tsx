c50db72959fa1301478d0d635fd06e69d045b997
Lennard Last <84354746+LennardLt@users.noreply.github.com>
2026-03-03 23:42:09 +0100

    add journal feature that lists journal entries

---
+++ src/components/journal/Journal.tsx
@@ -0,0 +1,50 @@
+import { useInteractiveBalanceData } from "../../context/InteractiveBalanceDataContext";
+import { useWindowManager } from "../../context/WindowManagerContext";
+import { sumLines } from "../../util/sumEntryLines";
+
+
+
+
+export function Journal() {
+    const { interactiveBalanceData } = useInteractiveBalanceData();
+    const { openWindow } = useWindowManager();
+
+    const journalEntries = interactiveBalanceData.journalEntries;
+
+    return (
+        <div>
+            <button
+                className="px-2 py-0.5 rounded bg-gray-100 hover:bg-gray-200 text-sm"
+                onClick={() => openWindow({ type: "JournalEntry", payload: { isDraft: true } })}
+                >
+                Neue Buchung
+            </button>
+            <table className="w-full table-fixed">
+                <thead>
+                    <tr className="text-left">
+                        <th className="w-1/12">ID</th>
+                        <th className="w-8/12">Beschreibung</th>
+                        <th className="w-3/12 text-right">Buchungsbetrag</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {journalEntries.map((entry) => {
+                        const debitLines = entry.entryLines.filter(line => line.entryType === "debit");
+                        const creditLines = entry.entryLines.filter(line => line.entryType === "credit");
+                        const debitSum = sumLines(debitLines);
+                        return (
+                            <tr
+                                className="cursor-pointer border hover:bg-blue-50 transition-colors duration-100"
+                                onClick={() => openWindow({ type: "JournalEntry", payload: { isDraft: false, id: entry.id } })}>
+                                <td>{entry.id}</td>
+                                <td>{entry.description}</td>
+                                <td className="text-right pr-1">{debitSum.toFixed(2)} €</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
+
+}
